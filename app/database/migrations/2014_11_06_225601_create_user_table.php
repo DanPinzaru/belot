2b185ce62cb0b744fbd2610a47c8535f654c5c2b
Dan Pinzaru <danpinzaru@Dans-MacBook-Pro.local>
2014-11-06 23:02:28 +0000

    Initial setup

---
+++ app/database/migrations/2014_11_06_225601_create_user_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		DB::statement("CREATE TABLE db_belot.user (
+  			id INT NOT NULL AUTO_INCREMENT,
+  			email VARCHAR(45) NOT NULL,
+  			first_name VARCHAR(45) NOT NULL,
+  			last_name VARCHAR(45) NOT NULL,
+  			dob DATE NOT NULL,
+  			type VARCHAR(45) NOT NULL,
+  			created_at TIMESTAMP NOT NULL,
+  			updated_at TIMESTAMP NOT NULL,
+  			remember_token VARCHAR(100) NOT NULL,
+  			PRIMARY KEY (id),
+  			UNIQUE INDEX email_UNIQUE (email ASC));");
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		DB::statement("DROP TABLE db_belot.user");
+	}
+
+}
